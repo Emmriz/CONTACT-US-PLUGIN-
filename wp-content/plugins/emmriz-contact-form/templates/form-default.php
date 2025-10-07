dd0270bd8701720027f5644d72e8ce701cb7a8ae
Emmanuel Ajolore <emmanuelajolore@gmail.com>
2025-10-07 15:00:26 +0100

    Updated the form-default.php

--- wp-content/plugins/emmriz-contact-form/templates/form-default.php
+++ wp-content/plugins/emmriz-contact-form/templates/form-default.php
@@ -50,7 +50,7 @@ $form_data = $form['data'];
                         <?php endif; ?>
                     </label>
                     
-                    <?php echo $this->render_form_field($field, $form['id']); ?>
+                    <?php echo ECF_Template_Helper::render_form_field($field, $form['id']); ?>
                 </div>
             <?php endif; ?>
         <?php endforeach; ?>
@@ -71,106 +71,3 @@ $form_data = $form['data'];
         <?php endif; ?>
     </form>
 </div>
-
-<?php
-/**
- * Render form field for frontend
- */
-function render_form_field($field, $form_id) {
-    $field_id = esc_attr($field['id']);
-    $field_name = esc_attr($field['id']);
-    $placeholder = esc_attr($field['placeholder']);
-    $required = $field['required'] ? 'required' : '';
-    $class = 'ecf-field-' . $field['type'];
-    
-    switch ($field['type']) {
-        case 'text':
-        case 'email':
-            return sprintf(
-                '<input type="%s" id="%s" name="%s" class="%s" placeholder="%s" %s>',
-                esc_attr($field['type']),
-                $field_id,
-                $field_name,
-                $class,
-                $placeholder,
-                $required
-            );
-        
-        case 'textarea':
-            return sprintf(
-                '<textarea id="%s" name="%s" class="%s" placeholder="%s" %s rows="4"></textarea>',
-                $field_id,
-                $field_name,
-                $class,
-                $placeholder,
-                $required
-            );
-        
-        case 'select':
-            $options = isset($field['options']) ? $field['options'] : array();
-            $options_html = '<option value="">' . $placeholder . '</option>';
-            
-            foreach ($options as $option) {
-                $options_html .= sprintf(
-                    '<option value="%s">%s</option>',
-                    esc_attr($option),
-                    esc_html($option)
-                );
-            }
-            
-            return sprintf(
-                '<select id="%s" name="%s" class="%s" %s>%s</select>',
-                $field_id,
-                $field_name,
-                $class,
-                $required,
-                $options_html
-            );
-        
-        case 'checkbox':
-        case 'radio':
-            $options = isset($field['options']) ? $field['options'] : array();
-            $wrapper_class = 'ecf-' . $field['type'] . '-group';
-            $options_html = '';
-            
-            foreach ($options as $index => $option) {
-                $option_id = $field_id . '_' . $index;
-                $options_html .= sprintf(
-                    '<div class="ecf-%s-item">
-                        <input type="%s" id="%s" name="%s[]" value="%s">
-                        <label for="%s">%s</label>
-                    </div>',
-                    $field['type'],
-                    $field['type'],
-                    $option_id,
-                    $field_name,
-                    esc_attr($option),
-                    $option_id,
-                    esc_html($option)
-                );
-            }
-            
-            return sprintf('<div class="%s">%s</div>', $wrapper_class, $options_html);
-        
-        case 'number':
-            return sprintf(
-                '<input type="number" id="%s" name="%s" class="%s" placeholder="%s" %s>',
-                $field_id,
-                $field_name,
-                $class,
-                $placeholder,
-                $required
-            );
-        
-        default:
-            return sprintf(
-                '<input type="text" id="%s" name="%s" class="%s" placeholder="%s" %s>',
-                $field_id,
-                $field_name,
-                $class,
-                $placeholder,
-                $required
-            );
-    }
-}
-?>
